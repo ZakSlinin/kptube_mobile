a46e2b8b6c140df8b3dbc7268e74beacded02181
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2025-05-17 14:44:27 +0300

    feat: bottom navigation bar size refactor

--- lib/features/home/widgets/bottom_navigation_bar/bottom_navigation_bar.dart
+++ lib/features/home/widgets/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -18,30 +18,57 @@ class BottomNavigationBarWidget extends StatefulWidget {
 class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
   @override
   Widget build(BuildContext context) {
-    return BottomNavigationBar(
-      currentIndex: widget.selectedPageIndex,
-      onTap: widget.onPageSelected,
-      type: BottomNavigationBarType.fixed,
-      items: <BottomNavigationBarItem>[
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            'assets/svg/Home.svg',
-            color: Colors.white,
+    return SizedBox(
+      height: 105,
+      child: BottomNavigationBar(
+        currentIndex: widget.selectedPageIndex,
+        onTap: widget.onPageSelected,
+        type: BottomNavigationBarType.fixed,
+        iconSize: 0,
+        selectedFontSize: 0,
+        unselectedFontSize: 0,
+        items: <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: const EdgeInsets.all(6),
+              alignment: Alignment.center,
+              width: 60,
+              height: 60,
+              child: SvgPicture.asset(
+                'assets/svg/Home.svg',
+                fit: BoxFit.contain,
+              ),
+            ),
+            label: '',
           ),
-          label: '',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset('assets/svg/Add_content.svg'),
-          label: '',
-        ),
-        BottomNavigationBarItem(
-          icon: SvgPicture.asset(
-            'assets/svg/User.svg',
-            color: Colors.white,
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: const EdgeInsets.all(6),
+              alignment: Alignment.center,
+              width: 60,
+              height: 60,
+              child: SvgPicture.asset(
+                'assets/svg/Add_content.svg',
+                fit: BoxFit.contain,
+              ),
+            ),
+            label: '',
           ),
-          label: '',
-        ),
-      ],
+          BottomNavigationBarItem(
+            icon: Container(
+              padding: const EdgeInsets.all(6),
+              alignment: Alignment.center,
+              width: 60,
+              height: 60,
+              child: SvgPicture.asset(
+                'assets/svg/User.svg',
+                fit: BoxFit.contain,
+              ),
+            ),
+            label: '',
+          ),
+        ],
+      ),
     );
   }
 }
