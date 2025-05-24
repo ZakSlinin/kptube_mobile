ef582302f4a3b7c144e3ff158baaca3a3c142987
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2025-05-25 01:56:13 +0300

    fix: height of bottom_navigation_bar.dart

--- lib/features/home/widgets/bottom_navigation_bar/bottom_navigation_bar.dart
+++ lib/features/home/widgets/bottom_navigation_bar/bottom_navigation_bar.dart
@@ -31,57 +31,49 @@ class _BottomNavigationBarWidgetState extends State<BottomNavigationBarWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 105,
-      child: BottomNavigationBar(
-        currentIndex: widget.selectedPageIndex,
-        onTap: _handleNavigation,
-        type: BottomNavigationBarType.fixed,
-        iconSize: 0,
-        selectedFontSize: 0,
-        unselectedFontSize: 0,
-        items: <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Container(
-              padding: const EdgeInsets.all(6),
-              alignment: Alignment.center,
-              width: 60,
-              height: 60,
-              child: SvgPicture.asset(
-                'assets/svg/Home.svg',
-                fit: BoxFit.contain,
-              ),
-            ),
-            label: '',
+    return BottomNavigationBar(
+      elevation: 25,
+      currentIndex: widget.selectedPageIndex,
+      onTap: _handleNavigation,
+      type: BottomNavigationBarType.fixed,
+      iconSize: 0,
+      selectedFontSize: 0,
+      unselectedFontSize: 0,
+      items: <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+          icon: Container(
+            padding: const EdgeInsets.all(6),
+            alignment: Alignment.center,
+            width: 60,
+            height: 60,
+            child: SvgPicture.asset('assets/svg/Home.svg', fit: BoxFit.contain),
           ),
-          BottomNavigationBarItem(
-            icon: Container(
-              padding: const EdgeInsets.all(6),
-              alignment: Alignment.center,
-              width: 60,
-              height: 60,
-              child: SvgPicture.asset(
-                'assets/svg/Add_content.svg',
-                fit: BoxFit.contain,
-              ),
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Container(
+            padding: const EdgeInsets.all(6),
+            alignment: Alignment.center,
+            width: 60,
+            height: 60,
+            child: SvgPicture.asset(
+              'assets/svg/Add_content.svg',
+              fit: BoxFit.contain,
             ),
-            label: '',
           ),
-          BottomNavigationBarItem(
-            icon: Container(
-              padding: const EdgeInsets.all(6),
-              alignment: Alignment.center,
-              width: 60,
-              height: 60,
-              child: SvgPicture.asset(
-                'assets/svg/User.svg',
-                fit: BoxFit.contain,
-              ),
-            ),
-            label: '',
+          label: '',
+        ),
+        BottomNavigationBarItem(
+          icon: Container(
+            padding: const EdgeInsets.all(6),
+            alignment: Alignment.center,
+            width: 60,
+            height: 60,
+            child: SvgPicture.asset('assets/svg/User.svg', fit: BoxFit.contain),
           ),
-        ],
-      ),
+          label: '',
+        ),
+      ],
     );
   }
 }
