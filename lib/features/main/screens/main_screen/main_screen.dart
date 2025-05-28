59e9b3bf10c4174c51a257d8675b06193f36a1c2
Zak Slinin <156526564+ZakSlinin@users.noreply.github.com>
2025-05-28 13:19:42 +0300

    refactor: search bar ui

--- lib/features/main/screens/main_screen/main_screen.dart
+++ lib/features/main/screens/main_screen/main_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:kptube_mobile/core/models/video/video.dart';
 import 'package:kptube_mobile/core/routing/app_router.dart';
 import 'package:kptube_mobile/core/widgets/video_grid_item.dart';
@@ -48,70 +49,81 @@ class _MainScreenState extends State<MainScreen> {
               onRefresh: () async {
                 context.read<MainBloc>().add(GetMainEvent());
               },
-              child: CustomScrollView(
-                slivers: [
-                  SliverAppBar(
-                    floating: true,
-                    snap: true,
-                    backgroundColor:
-                        Theme.of(context).appBarTheme.backgroundColor,
-                    surfaceTintColor: Colors.transparent,
-                    elevation: 0,
-                    bottom: PreferredSize(
-                      preferredSize: const Size.fromHeight(80),
-                      child: Center(
-                        child: Container(
-                          width: 350,
-                          height: 60,
-                          margin: EdgeInsets.all(10),
-                          decoration: BoxDecoration(
-                            color: Color.fromARGB(255, 177, 177, 177),
-                            borderRadius: BorderRadius.circular(16),
-                          ),
-                          child: Row(
-                            children: [
-                              Icon(Icons.search_rounded, size: 40),
-                              SizedBox(width: 16),
-                              Expanded(
-                                child: TextField(
-                                  style: TextStyle(color: Colors.black),
-                                  decoration: InputDecoration(
-                                    hintText: 'search video',
+
+              child: GestureDetector(
+                onTap: () => FocusManager.instance.primaryFocus?.unfocus(),
+                child: CustomScrollView(
+                  slivers: [
+                    SliverAppBar(
+                      floating: true,
+                      snap: true,
+                      backgroundColor:
+                      Theme.of(context).appBarTheme.backgroundColor,
+                      surfaceTintColor: Colors.transparent,
+                      elevation: 0,
+                      bottom: PreferredSize(
+                        preferredSize: const Size.fromHeight(20),
+                        child: Center(
+                          child: Container(
+                            width: 350,
+                            height: 40,
+                            margin: EdgeInsets.all(10),
+                            decoration: BoxDecoration(
+                              color: Color.fromARGB(255, 3, 3, 3),
+                              borderRadius: BorderRadius.circular(16),
+                            ),
+                            child: Row(
+                              children: [
+                                const SizedBox(width: 16),
+                                SvgPicture.asset(
+                                  'assets/svg/Search.svg',
+                                  fit: BoxFit.contain,
+                                  colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+                                ),
+                                const SizedBox(width: 16),
+                                const Expanded(
+                                  child: TextField(
+                                    style: TextStyle(color: Colors.white),
+                                    decoration: InputDecoration(
+                                      hintText: 'Введите запрос',
+                                      hintStyle: TextStyle(color: Colors.grey),
+                                      border: InputBorder.none,
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                  SliverPadding(
-                    padding: const EdgeInsets.all(16),
-                    sliver: SliverGrid(
-                      gridDelegate:
-                          const SliverGridDelegateWithFixedCrossAxisCount(
-                            crossAxisCount: 1,
-                            childAspectRatio: 1.2,
-                            crossAxisSpacing: 8,
-                            mainAxisSpacing: 8,
-                          ),
-                      delegate: SliverChildBuilderDelegate((context, index) {
-                        final video = state.videos[index];
-                        return VideoGridItem(
-                          key: ValueKey('video_${video.Video_ID}'),
-                          video: video,
-                          onTap: () {
-                            print('${video.name} tapped');
-                            context.read<MainBloc>().add(
-                              VideoTap(Video_ID: video.Video_ID!),
-                            );
-                          },
-                        );
-                      }, childCount: state.videos.length),
+                    SliverPadding(
+                      padding: const EdgeInsets.all(16),
+                      sliver: SliverGrid(
+                        gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 1,
+                          childAspectRatio: 1.2,
+                          crossAxisSpacing: 8,
+                          mainAxisSpacing: 8,
+                        ),
+                        delegate: SliverChildBuilderDelegate((context, index) {
+                          final video = state.videos[index];
+                          return VideoGridItem(
+                            key: ValueKey('video_${video.Video_ID}'),
+                            video: video,
+                            onTap: () {
+                              print('${video.name} tapped');
+                              context.read<MainBloc>().add(
+                                VideoTap(Video_ID: video.Video_ID!),
+                              );
+                            },
+                          );
+                        }, childCount: state.videos.length),
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             );
           }
